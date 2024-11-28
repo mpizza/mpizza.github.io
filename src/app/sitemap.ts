eacc21054f7506d5790bb78445c48d6def723605
PYChen <mmm198219@gmail.com>
2024-11-28 17:47:58 +0800

    Add new article and sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -24,6 +24,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'weekly',
       priority: 0.5,
     },
+    {
+      url: `${BASE_URL}/shop`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.5,
+    },
   ];
 
   const posts = getAllPosts();
